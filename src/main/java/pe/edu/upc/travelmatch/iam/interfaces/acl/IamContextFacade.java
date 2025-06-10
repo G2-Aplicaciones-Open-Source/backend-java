cb289d9adbda91b0d3f94695c36bae547112202a
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-06-10 17:10:27 -0500

    refactor(profiles): modify acl interfaces and review command service

--- src/main/java/pe/edu/upc/travelmatch/iam/interfaces/acl/IamContextFacade.java
+++ src/main/java/pe/edu/upc/travelmatch/iam/interfaces/acl/IamContextFacade.java
@@ -1,16 +1,22 @@
 package pe.edu.upc.travelmatch.iam.interfaces.acl;
 
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.stereotype.Service;
 import pe.edu.upc.travelmatch.iam.domain.model.commands.SignUpCommand;
 import pe.edu.upc.travelmatch.iam.domain.model.entities.Role;
 import pe.edu.upc.travelmatch.iam.domain.model.queries.GetUserByEmailQuery;
 import pe.edu.upc.travelmatch.iam.domain.model.queries.GetUserByIdQuery;
 import pe.edu.upc.travelmatch.iam.domain.services.UserCommandService;
 import pe.edu.upc.travelmatch.iam.domain.services.UserQueryService;
+import pe.edu.upc.travelmatch.iam.interfaces.rest.resources.UserResource;
+import pe.edu.upc.travelmatch.iam.interfaces.rest.transform.UserResourceFromEntityAssembler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
+@Service
 public class IamContextFacade {
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
@@ -28,7 +34,7 @@ public class IamContextFacade {
         return result.get().getId();
     }
 
-    public Long createUser(String email, String password, String firstName, String lastName, String phone,List<String> roleNames) {
+    public Long createUser(String email, String password, String firstName, String lastName, String phone, List<String> roleNames) {
         if (roleNames == null) roleNames = new ArrayList<>();
         var roles = roleNames.stream()
                 .map(Role::toRoleFromName)
@@ -40,11 +46,26 @@ public class IamContextFacade {
         return result.get().getId();
     }
 
+    public Optional<UserResource> fetchUserById(Long userId) {
+        var getUserByIdQuery = new GetUserByIdQuery(userId);
+        var result = userQueryService.handle(getUserByIdQuery);
+        if(result.isEmpty()) return Optional.empty();
+        var userResource = UserResourceFromEntityAssembler.toResourceFromEntity(result.get());
+        return Optional.of(userResource);
+    }
+
     public Long fetchUserIdByEmail(String email) {
         var getUserByEmailQuery = new GetUserByEmailQuery(email);
         var result = userQueryService.handle(getUserByEmailQuery);
         if (result.isEmpty()) return 0L;
         return result.get().getId();
+    }
+
+    public boolean existsUserByEmailAndIdIsNot(String email, Long id) {
+        var getUserByEmailQuery = new GetUserByEmailQuery(email);
+        var result = userQueryService.handle(getUserByEmailQuery);
+        if (result.isEmpty()) return false;
+        return !Objects.equals(result.get().getId(), id);
     }
 
     public String fetchEmailByUserId(Long userId) {
--- src/main/java/pe/edu/upc/travelmatch/profiles/domain/services/ReviewCommandService.java
+++ src/main/java/pe/edu/upc/travelmatch/profiles/domain/services/ReviewCommandService.java
@@ -1,11 +1,14 @@
 package pe.edu.upc.travelmatch.profiles.domain.services;
 
+import pe.edu.upc.travelmatch.profiles.domain.model.aggregates.Review;
 import pe.edu.upc.travelmatch.profiles.domain.model.commands.CreateReviewCommand;
 import pe.edu.upc.travelmatch.profiles.domain.model.commands.DeleteReviewCommand;
 import pe.edu.upc.travelmatch.profiles.domain.model.commands.UpdateReviewCommand;
 
+import java.util.Optional;
+
 public interface ReviewCommandService {
-    void handle(CreateReviewCommand command);
-    void handle(UpdateReviewCommand command);
+    Long handle(CreateReviewCommand command);
+    Optional<Review> handle(UpdateReviewCommand command);
     void handle(DeleteReviewCommand command);
 }
