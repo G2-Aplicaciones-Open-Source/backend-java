3d94f78b1d66eb43fb32514c96eb66a444133f25
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-07-04 22:53:47 -0500

    feat(bookings): add command and query service contracts

---
+++ src/main/java/pe/edu/upc/travelmatch/bookings/domain/services/BookingCommandService.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.travelmatch.bookings.domain.services;
+
+import pe.edu.upc.travelmatch.bookings.domain.model.aggregates.Booking;
+import pe.edu.upc.travelmatch.bookings.domain.model.commands.*;
+
+import java.util.Optional;
+
+public interface BookingCommandService {
+    Long handle(CreateBookingCommand command);
+    Optional<Booking> handle(CancelBookingCommand command);
+    Long handle(ProcessPaymentCommand command);
+    boolean handle(FailPaymentCommand command);
+    Long handle(ProcessPayoutCommand command);
+    boolean handle(CompletePayoutCommand command);
+    Long handle(InitiateRefundCommand command);
+}
