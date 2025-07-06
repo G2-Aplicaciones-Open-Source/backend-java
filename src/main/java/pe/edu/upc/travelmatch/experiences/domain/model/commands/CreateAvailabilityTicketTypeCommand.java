2c3cc40a12432466bce4a94c2e9074a72843462a
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-07-06 12:50:26 -0500

    refactor(experiences): update commands

--- src/main/java/pe/edu/upc/travelmatch/experiences/domain/model/commands/CreateAvailabilityTicketTypeCommand.java
+++ src/main/java/pe/edu/upc/travelmatch/experiences/domain/model/commands/CreateAvailabilityTicketTypeCommand.java
@@ -1,17 +1,11 @@
 
 package pe.edu.upc.travelmatch.experiences.domain.model.commands;
 
-import pe.edu.upc.travelmatch.experiences.domain.model.valueobjects.TicketType;
-
-import java.math.BigDecimal;
-import pe.edu.upc.travelmatch.experiences.domain.model.aggregates.Availability;
-import pe.edu.upc.travelmatch.experiences.domain.model.valueobjects.TicketType;
 import java.math.BigDecimal;
 
 public record CreateAvailabilityTicketTypeCommand(
-        Availability availability,
-        Long ticketTypeId,
-        TicketType ticketType,
-        BigDecimal price,
-        int stock
+       Long availabilityId,
+       Long ticketTypeId,
+       BigDecimal price,
+       int stock
 ) {}
